175c881c82d91a3ab92a0da5be3cb69d2e441c99
Antonio Izaguirre <antoniza.developer@gmail.com>
2022-10-03 17:48:43 -0600

    Subida de la tarea

---
+++ Ejercicio1_1/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+//Hecho por Antonio Izaguirre (Antoniza) (202010020281)
+namespace Ejercicio1_1
+{
+    public partial class MainPage : ContentPage
+    {
+        Double numb1, numb2, result;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void AddUp()
+        {
+            numb1 = Convert.ToDouble(num1.Text);
+            numb2 = Convert.ToDouble(num2.Text);
+
+            result = numb1 + numb2;
+        }
+
+        private void Subtract()
+        {
+            numb1 = Convert.ToDouble(num1.Text);
+            numb2 = Convert.ToDouble(num2.Text);
+
+            result = numb1 - numb2;
+        }
+
+        private void Multiply()
+        {
+            numb1 = Convert.ToDouble(num1.Text);
+            numb2 = Convert.ToDouble(num2.Text);
+
+            result = numb1 * numb2;
+        }
+
+        private void Divide()
+        {
+            numb1 = Convert.ToDouble(num1.Text);
+            numb2 = Convert.ToDouble(num2.Text);
+
+            result = numb1 / numb2;
+        }
+
+        private async void btnAdd_Clicked(object sender, EventArgs e)
+        {
+            AddUp();
+            await Navigation.PushAsync(new resultPage(result));
+        }
+
+        private async void btnSub_Clicked(object sender, EventArgs e)
+        {
+            Subtract();
+            await Navigation.PushAsync(new resultPage(result));
+        }
+
+        private async void btnMul_Clicked(object sender, EventArgs e)
+        {
+            Multiply();
+            await Navigation.PushAsync(new resultPage(result));
+        }
+
+        private async void btnDiv_Clicked(object sender, EventArgs e)
+        {
+            Divide();
+            await Navigation.PushAsync(new resultPage(result));
+        }
+    }
+}
---
+++ Ejercicio1_1/resultPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+//Hecho por Antonio Izaguirre (Antoniza) (202010020281)
+namespace Ejercicio1_1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class resultPage : ContentPage
+    {
+        public resultPage(Double result)
+        {
+            InitializeComponent();
+            txtResult.Text = result.ToString();
+        }
+    }
+}
